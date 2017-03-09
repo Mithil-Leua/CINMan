8b6691e531bef7cc377b089fae29170aec582d11
Mithil Leua <mithilleua@gmail.com>
2017-03-09 18:19:10 +0530

    Update background.py

--- background.py
+++ background.py
@@ -2,6 +2,8 @@ import time
 import socket
 
 #client side socket
+#change the host , temporarily set to 10.6.15.113 
+# host = address on which server is running.
 sock = socket.socket()
 host = "10.6.15.113"
 port = 12345
