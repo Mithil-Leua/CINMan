8931fd7c8d6fec8e473af6b5bf0ac5fc9275a13a
Mithil Leua <mithilleua@gmail.com>
2017-03-09 18:17:31 +0530

    Add files via upload

--- background.py
+++ background.py
@@ -1,8 +1,12 @@
 import time
 import socket
 
+#client side socket
 sock = socket.socket()
-host = socket.gethostname()
-port = 4567
+host = "10.6.15.113"
+port = 12345
 
 sock.connect((host,port))
+
+import cinmantest
+sock.send(cinmantest.client_data.encode())
