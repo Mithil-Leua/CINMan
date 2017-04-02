da957d51fa866bf0319f8c7c31d2c077bf19c2e8
Premnath7 <premnath1908@gmail.com>
2017-04-02 19:18:42 +0530

    Update devices.py

--- devices.py
+++ devices.py
@@ -1,1 +1,25 @@
-
+def find_between( s, first, last ):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+with file("out.txt") as f:
+		old = f.read()
+x=[]
+while True:
+	with file("out.txt") as f:
+		s = f.read()
+	olds=s
+	s=s.replace(old,"")
+	if( s):
+		if("detected" in s):
+			x.append(["connected at "+s[:15],find_between(s,"Vendor=",","),find_between(s,"idProduct=","\n"),find_between(s,"Product: ","\n"),find_between(s,"SerialNumber: ","\n"),find_between(s,"device number "," ")])
+			print x
+		if("disconnect" in s):
+			x.append(["disconnected at "+s[:15],find_between(s,"device number ","\n")])
+			print x
+	f.close()
+	old=olds
+	time.sleep(1)
